7fac04416b8f46a87d200bc3f14c9ae9337aee6e
Adarsh Kesharwani <adarsh2301k@gmail.com>
2025-09-09 17:54:37 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app=express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 connectDB();
 
 app.use("/api/auth", authRoute);
